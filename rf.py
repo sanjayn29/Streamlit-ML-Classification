4a483636c04832ae38ec704653d2da49df757c24
Sanjay N <sanjayn29.aiml@gmail.com>
2025-11-24 10:51:33 +0530

    rf

---
+++ rf.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import numpy as np
+import joblib
+
+st.title("🍎 Apple Quality Prediction BY Random Forest Model")
+
+st.write("Enter the apple characteristics below to predict Good or Bad Apple.")
+
+# Load model and scaler using JOBLIB
+model = joblib.load("rf_model.pkl")
+
+size = st.number_input("Size")
+weight = st.number_input("Weight")
+sweetness = st.number_input("Sweetness")
+crunchiness = st.number_input("Crunchiness")
+juiciness = st.number_input("Juiciness")
+ripeness = st.number_input("Ripeness")
+acidity = st.number_input("Acidity")
+
+if st.button("Predict"):
+    X = np.array([[size, weight, sweetness, crunchiness, juiciness, ripeness, acidity]])
+
+    pred = model.predict(X)[0]
+
+    result = "Good 🍏" if pred == 1 else "Bad 🍎"
+    st.success(f"Prediction: **{result}**")
